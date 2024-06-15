22591e8229e458eeca4d3e6484e3ace35ba7508c
adriansito <adriancrosby02@gmail.com>
2024-06-14 21:19:35 -0700

    initial commit

---
+++ dist/main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('hey i kiddnda wsdsdsdsssdorkk')
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdC1wYWdlLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCdoZXkgaSBraWRkbmRhIHdzZHNkc2Rzc3Nkb3JraycpXG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
